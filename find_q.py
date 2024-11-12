3ec50fd19f2a95e6de41a1c8c885d023cae00d3f
horoling <97731535+horoling@users.noreply.github.com>
2024-11-12 21:26:51 +0800

    Update find_q.py

--- find_q.py
+++ find_q.py
@@ -1,4 +1,4 @@
-from newf import vf2
+from newf import vf2_emb
 
 source = 'graphdata/pubchem_no_H.txt'
 target = 'graphdata/demoquery.txt'
@@ -8,15 +8,26 @@ k = 10
 
 result = []
 result_set = set()
+time = 0
 
 while len(result) < k:
-    tmp = vf2(source, target, δ)
-    for item in tmp:
+    inc = 0
+    print(δ, inc,time)
+    tmp = vf2_emb(source, target, δ)
+    for item in tmp[1]:
         if item not in result_set:
             result_set.add(item)
             result.append(item)
+            inc += 1
             if len(result) >= k:
                 break
-    δ += 1
+    if inc <= k/10:
+        δ += 2
+        time += 1   
+        if time >= 2:
+            δ += 2
+    else:
+        δ += 1
+        time = 0
 
 print(result)
